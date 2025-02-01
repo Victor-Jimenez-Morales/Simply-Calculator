b04f4ac77fef9c7870e392db1caccd73545ce98e
Victor <victor.jimenez6500@gmail.com>
2025-02-01 22:26:48 +0000

    add history function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 // Calculator display input
 let display = document.getElementById("display");
+let operationCalculated = false;
 
 function appendToDisplay(param) {
-    let lastInput = display.value.slice(-1);
+    if (operationCalculated) {
+        display.value = "";
+        operationCalculated = false;
+    }
 
     // Handle operators and colons
+    let lastInput = display.value.slice(-1);
     if ((["x", "/", "+", "-"].includes(param) && ["x", "/", "+", "-"].includes(lastInput)) || (lastInput == "." && param == ".")) {
         deleteLast();
     }
@@ -20,44 +25,23 @@ function deleteLast() {
     display.value = display.value.slice(0, -1);
 }
 
-addEventListener("keydown", function (event) {
-    if (event.key === "Backspace") {
-        deleteLast();
-    }
-});
-
 function convertPercentage() {
     const regex = /[+\-x/]/;
-    let lastNumber = display.value.split(regex).pop();
+    const lastNumber = display.value.split(regex).pop();
     if (lastNumber != "") {
         display.value += "/100";
     }
 }
 
 function calculateResult() {
-    const regex = /([+\-x/])/;
-    let input = display.value.split(regex).map((item) => (item === "x" ? "*" : item));
-    console.log(input);
-    if (input[0] === "" && input.length === 1) {
-        display.value = 0;
-    } else {
-        // Handle '*' and '/' first
-        let result = processOperators(input, ["*", "/"]);
-
-        // Handle '+' and '-' next
-        result = processOperators(result, ["+", "-"]);
-
-        display.value = isNaN(result) ? "Err" : result;
-    }
-
     function processOperators(array, operators) {
         const result = [];
         let i = 0;
 
         while (i < array.length) {
             if (operators.includes(array[i])) {
-                let leftNum = parseFloat(result.pop()); // Previous number
-                let rightNum = parseFloat(array[i + 1]); // Next number
+                const leftNum = parseFloat(result.pop()); // Previous number
+                const rightNum = parseFloat(array[i + 1]); // Next number
 
                 // Perform the operation
                 let calc;
@@ -86,4 +70,66 @@ function calculateResult() {
 
         return result;
     }
+
+    const operation = display.value;
+    const regex = /([+\-x/])/;
+    const input = operation.split(regex).map((item) => (item === "x" ? "*" : item));
+
+    if (input[0] === "" && input.length === 1) {
+        display.value = 0;
+    } else {
+        // Handle '*' and '/' first
+        let result = processOperators(input, ["*", "/"]);
+
+        // Handle '+' and '-' next
+        result = processOperators(result, ["+", "-"]);
+
+        display.value = isNaN(result) ? "Err" : result;
+
+        addToHistory(operation, result);
+    }
+
+    operationCalculated = true;
 }
+
+function getHistory() {
+    const history = document.querySelector("#history-list");
+
+    if (localStorage.length == 0) {
+        const element = document.createElement("p");
+
+        element.textContent = "There isn't any operations yet!";
+
+        history.append(element);
+    } else {
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            const element = document.createElement("p");
+
+            element.classList.add("history-line");
+            element.textContent = `${key} = ${value}`;
+            history.prepend(element);
+        }
+    }
+}
+
+function addToHistory(operation, result) {
+    const history = document.querySelector("#history-list");
+    const element = document.createElement("p");
+
+    localStorage.setItem(operation, result);
+
+    element.classList.add("history-line");
+    element.textContent = `${operation} = ${result}`;
+    history.prepend(element);
+    // getHistory();
+}
+
+addEventListener("keydown", function (event) {
+    if (event.key === "Backspace") {
+        deleteLast();
+    }
+});
+
+getHistory();
